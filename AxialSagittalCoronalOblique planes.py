199eb17e5cc57d01c3e164bd6daccedf48b22c23
AhmedM20 <55909284+AhmedM20@users.noreply.github.com>
2022-12-22 23:47:47 +0200

    Update AxialSagittalCoronalOblique planes.py

--- AxialSagittalCoronalOblique planes.py
+++ AxialSagittalCoronalOblique planes.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import os
 import pydicom as dicom
 from functools import partial
+from scipy import ndimage, misc
+import math
 
 class Ui_MainWindow(QMainWindow):
     def __init__(self):
@@ -67,6 +69,7 @@ class Ui_MainWindow(QMainWindow):
         self.oblique.showAxis('bottom', False)
         self.oblique.showAxis('left', False)
         self.oblique.invertY(True)
+        self.oblique.setMouseEnabled(x=False,y=False)
         self.selectors = [self.axial,self.sagittal,self.coronal,self.oblique]
         self.firstRow.addWidget(self.axial)
         self.firstRow.addWidget(self.sagittal)
@@ -111,38 +114,81 @@ class Ui_MainWindow(QMainWindow):
         self.volume3d=np.zeros(imgShape)
 
         #fill 3D array with  the images from the files
+        sx, sy, sz = self.volume3d.shape
+
+        #print(self.volume3d.shape)
+        #print(self.rotation.value())
         for i,s in enumerate(slices):
             array2D=s.pixel_array
             self.volume3d[:,:,i]= array2D
         
-        self.images[0] = self.volume3d[:,:,i//2]
+        self.images[0] = self.volume3d[:,:,sz//2]
    
-        self.images[1] = self.volume3d[:,i//2,:]
+        self.images[1] = self.volume3d[:,sy//2,:]
     
-        self.images[2] = self.volume3d[i//2,:,:]
-   
+        self.images[2] = self.volume3d[sx//2,:,:]
+
+        self.images[3] = np.zeros((sx,sy))
 
         self.update()
         self.drawLines()
-        
-   
-
-
-    def newimg(self,i,q,slicevalue):
+        #print(self.lines)
+        points=self.get_line(list(self.lines[0][2].value()))
+        if not points:
+            if self.rotation.value() == 90:
+                self.images[3] = self.volume3d[sx//2,:,:]
+            elif self.rotation.value() == 0:
+                self.images[3] = self.volume3d[:,sy//2,:]
+        self.images[3]=self.volume3d[tuple(np.transpose(points))]
+        self.update()
+    def get_line(self,coords):
+        if len(coords) != 2:
+            return
+        x1,y1 = coords[0],coords[1]
+        sx, sy, sz = self.volume3d.shape
+        x2 = 0
+        y2=round(math.tan(math.radians(self.rotation.value()))*(x2-x1)+y1)
+        if y2 > 0:
+            x3=sx-1
+            y3=round(math.tan(math.radians(self.rotation.value()))*(x3-x1)+y1)
+            print("Y2"+str(y2))
+            
+        else:
+            y2=0
+            x2=round((y2-y1)/math.tan(math.radians(self.rotation.value()))+x1)
+            y3=sy-1
+            x3=round((y3-y1)/math.tan(math.radians(self.rotation.value()))+x1)
+        lineLength = np.sqrt(pow(x3 -x2, 2) + pow(y3 -y2, 2))
+        points = []
+        for pt in range(int(lineLength)-1):
+            inc = pt / lineLength
+            v1 = max(round((1-inc) * y2 + inc* y3),0)
+            v1 = min(v1,sx-1)
+            v2 = max(round((1-inc)*x2 + inc* x3),0)
+            v2 = min(v2,sy-1)
+            points.append([v1,v2])
+        return points
+    def newimg(self,i,q,slicevalue,slicevalue2=0):
 
         if i==0: # iam moving line in axial plane
          if q==0:  # vertical
           self.images[1] = self.volume3d[:,slicevalue,:] # sagital
          if q==1:   # horizontal
           self.images[2] = self.volume3d[slicevalue,:,:] # coronal
-
+         if q==2:   # oblique
+          points=self.get_line([slicevalue,slicevalue2])
+          if not points:
+            if self.rotation.value() == 90:
+                self.images[3] = self.volume3d[sx//2,:,:]
+            elif self.rotation.value() == 0:
+                self.images[3] = self.volume3d[:,sy//2,:]
+          self.images[3] = self.volume3d[tuple(np.transpose(points))]# oblique
         if i==1: #moving lines in sagital 
             if q==0: # vertival  moved
                 self.images[2] = self.volume3d[slicevalue,:,:] # coronal changed
             if q==1: # horizontal moved 
                 self.images[0]=self.volume3d[:,:,self.volume3d.shape[2]-1-slicevalue] # axial changed
-
-
+          
         if i ==2: # iam moving lines in coronal plane
             if q==0: #vertical moved
                 self.images[1]= self.volume3d[:,slicevalue,:] # sagital changed
@@ -152,13 +198,12 @@ class Ui_MainWindow(QMainWindow):
         self.updateOne()
 
     def updateOne(self):
-        for i in range(3):
+        for i in range(4):
             if i == 0:
                 self.ims[i].setImage(self.images[i])
             else:
                 self.ims[i].setImage(np.rot90(self.images[i]))
                 self.selectors[i].autoRange(padding=0)
-
     def drawLines(self):
         
         for i in range(3):
@@ -166,13 +211,14 @@ class Ui_MainWindow(QMainWindow):
             self.lines[i].append(pg.InfiniteLine(pos= self.images[i].shape[1]//2,bounds=(0,self.images[i].shape[1]-1),movable=True, angle=0,pen=pg.mkPen('g', width=2)))
             self.lines[i].append(pg.InfiniteLine(pos = QtCore.QPointF(0,0),movable=True, angle=45,pen=pg.mkPen('r', width=2)))
             for j,x in enumerate(self.lines[i]):
-                
-                x.sigPositionChanged.connect(partial(self.updateimg,i,j))
+                if j != 2:
+                    x.sigPositionChanged.connect(partial(self.updateimg,i,j))
+                else:
+                    x.sigPositionChangeFinished.connect(partial(self.updateimg,i,j))
                 self.selectors[i].addItem(x)
         self.lines[1][2].setVisible(False)
         self.lines[2][2].setVisible(False)
     def getPos(self,i,j):
-        # print(self.lines[i][j].getPos())
         return (self.lines[i][j].getPos())
 
 
@@ -182,11 +228,15 @@ class Ui_MainWindow(QMainWindow):
         x=self.lines[i][j].getPos()
         if j==1:  # horizontal
           ne=round(x[1]) # y value
+          self.newimg(i,j,ne)
         if j==0:  #vertical
             ne=round(x[0])  # x value
+            self.newimg(i,j,ne)
+        if j==2:    
+            self.newimg(i,j,round(x[0]),round(x[1]))
+            # return
       
-      
-        self.newimg(i,j,ne)
+        
             
         
     
@@ -196,8 +246,7 @@ class Ui_MainWindow(QMainWindow):
         getValue = self.rotation.value()
         self.lines[0][2].setAngle(getValue)
     def update(self):
-        for x in range(3):
-            #self.selectors[x].clear()
+        for x in range(4):
             img = self.ims[x]
             self.selectors[x].addItem(img)
             self.selectors[x].setXRange(min=0, max=self.images[x].shape[0], padding=0)
